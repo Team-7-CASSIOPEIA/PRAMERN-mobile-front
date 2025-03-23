a043dd6884de4e61c379353c47a8b0de5f03b3de
IceKspRt <152171925+IceKspRt@users.noreply.github.com>
2025-03-23 22:25:39 +0700

    Home and Form

--- lib/features/home/home_controller.dart
+++ lib/features/home/home_controller.dart
@@ -4,15 +4,15 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class HomeController {
-
   Future<Map<String, String>?> fetchUserData() async {
     final prefs = await SharedPreferences.getInstance();
     final String? userId = prefs.getString('user_id');
     final String? accessToken = prefs.getString('accessToken');
-    
+
     if (userId == null || accessToken == null) return null;
 
-    final String apiUrl = '${dotenv.env['API_URL']}/api/user/$userId';  // Adjust API URL
+    final String apiUrl =
+        '${dotenv.env['API_URL']}/api/user/$userId'; // Adjust API URL
 
     try {
       final response = await http.get(
@@ -22,7 +22,6 @@ class HomeController {
           'Content-Type': 'application/json',
         },
       );
-        
 
       if (response.statusCode == 200) {
         final data = jsonDecode(response.body);
@@ -35,6 +34,63 @@ class HomeController {
     } catch (e) {
       print(e);
       return null;
+    }
+  }
+
+  Future<List<Map<String, dynamic>>?> fetchEvaluationData() async {
+    final prefs = await SharedPreferences.getInstance();
+    final String? userId = prefs.getString('user_id');
+    final String? accessToken = prefs.getString('accessToken');
+
+    if (userId == null || accessToken == null) return null;
+
+    final String apiUrl =
+        '${dotenv.env['API_URL']}/api/assigns/$userId'; // Adjust API URL
+
+    try {
+      final response = await http.get(
+        Uri.parse(apiUrl),
+        headers: {
+          'Authorization': 'Bearer $accessToken',
+          'Content-Type': 'application/json',
+        },
+      );
+
+      if (response.statusCode == 200) {
+        final List<dynamic> assignData = jsonDecode(response.body);
+        List<Map<String, dynamic>> assignItems = [];
+
+        for (var assign in assignData) {
+          final evalId = assign['eval_id'];
+          final formId = assign['form_id'];
+
+          for (var assignee in assign['assignees']) {
+            if (assign['assessor']['_id'] == userId) {
+              assignItems.add({
+                'assign_id': assign['_id'],
+                'eval_id': evalId['_id'],
+                'eval_name': evalId['eval_name'],
+                'eval_template_id': formId['_id'],
+                'eval_template': formId['form_name'],
+                'assignee':
+                    '${assignee['assignee']['user_fname']} ${assignee['assignee']['user_lname']}',
+                'assignee_id': assignee['assignee']['_id'],
+                'assign_status': assignee['status'],
+                'round_number': assign['round_number'],
+                'is_available': assign['isAvailable'],
+              });
+            }
+          }
+        }
+
+        return assignItems;
+      } else {
+        // Handle the case where the response status is not 200
+        return [];
+      }
+    } catch (e) {
+      print(e);
+      return [];
     }
   }
 }
